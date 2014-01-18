0ff59b42ce29faf63ce1e6103fc8a87bb8e864c1
confuser <jamesmortemore@gmail.com>
2014-01-18 12:09:33 +0000

    Added auto updater and metrics

--- src/me/confuser/offlineplayer/OfflinePlayerPlugin.java
+++ src/me/confuser/offlineplayer/OfflinePlayerPlugin.java
@@ -1,11 +1,14 @@
 package me.confuser.offlineplayer;
 
 import java.io.File;
+import java.io.IOException;
 
 import me.confuser.offlineplayer.listeners.InventoryEvents;
+import net.gravitydevelopment.updater.Updater;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.MetricsLite;
 
 public class OfflinePlayerPlugin extends JavaPlugin {
 	private static OfflinePlayerPlugin statPlugin;
@@ -14,9 +17,22 @@ public class OfflinePlayerPlugin extends JavaPlugin {
 	public void onEnable() {
 		statPlugin = this;
 		playerWorldFolder = new File(Bukkit.getWorlds().get(0).getWorldFolder(), "players");
-		
+
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+
+		if (getConfig().getBoolean("autoUpdate"))
+			new Updater(this, 70666, getFile(), Updater.UpdateType.DEFAULT, false);
+
+		try {
+			MetricsLite metrics = new MetricsLite(this);
+			metrics.start();
+		} catch (IOException e) {
+			// Failed to submit the stats :-(
+		}
+
 		getCommand("offline").setExecutor(new CommandHandler("offline"));
-		
+
 		getServer().getPluginManager().registerEvents(new InventoryEvents(), this);
 	}
 
